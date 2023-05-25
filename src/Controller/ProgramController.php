9c354ee8e205cbbf0febcd85a5f63932e942e1a8
Gigic337 <124678071+Gigic337@users.noreply.github.com>
2023-05-25 16:13:18 +0200

    Adding show method in category controller and the view linked

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,23 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findall();
+
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+            'programs' => $programs
         ]);
     }
 
-    #[Route('/{id}', name: 'show', requirements: ['id' =>'\d+'])]
-    public function show(int $id): Response
+    #[Route('/show/{id<^[0-9]+$>}', name: 'show')]
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw  $this->CreateNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 
