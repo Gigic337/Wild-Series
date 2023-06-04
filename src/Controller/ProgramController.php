76b69a3ea269058edbef840e092c2ee0c7b93b7d
Gigic337 <124678071+Gigic337@users.noreply.github.com>
2023-06-04 10:22:57 +0200

    quete 16

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,9 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Entity\Season;
+use App\Entity\Episode;
 use App\Entity\Category;
 use App\Form\ProgramType;
 use App\Repository\CategoryRepository;
@@ -12,10 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Program;
-use App\Entity\Season;
-use App\Entity\Episode;
-
+use Symfony\Component\Validator\Constraints\Length; // Mise à jour de la référence
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
@@ -23,61 +23,63 @@ class ProgramController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(ProgramRepository $programRepository): Response
     {
-        $programs = $programRepository->findall();
+        $programs = $programRepository->findAll();
 
         return $this->render('program/index.html.twig', [
             'programs' => $programs
         ]);
     }
+
     #[Route('/new', name: 'new')]
-    public function new(Request $request, ProgramRepository $programRepository) : Response
+    public function new(Request $request, ProgramRepository $programRepository): Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $programRepository->save($program, true);
 
-            // Redirect to category list
+            // Redirect to program list
             return $this->redirectToRoute('program_index');
         }
 
         // Render the form
         return $this->render('program/new.html.twig', [
-            'form' => $form,
+            'form' => $form->createView(), // Utilisez createView() pour passer le formulaire à la vue
         ]);
     }
 
-    #[Route('/program{id}/', name: 'show')]
+    #[Route('/{id}', name: 'show')] // Supprimé le "program" avant "{id}"
     public function show(Program $program): Response
     {
-        return $this->render('program/show.html.twig', ['program' => $program]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id found in the program\'s table.'
+            );
+        }
+
+        return $this->render('program/show.html.twig', [
+            'program' => $program
+        ]);
     }
 
     #[Route('/{program}/seasons/{season}', name: 'season_show')]
-    public function showSeason(
-        Program $program,
-        Season $seasons): Response
+    public function showSeason(Program $program, Season $season): Response // Renommé "$seasons" en "$season"
     {
         return $this->render('program/season_show.html.twig', [
-            'seasons' => $seasons,
-            'program' => $program]);
-
+            'season' => $season, // Renommé "seasons" en "season"
+            'program' => $program
+        ]);
     }
 
     #[Route('/{program}/seasons/{season}/episodes/{episode}', name: 'episode_show')]
-    public function showEpisode(
-        Program $program,
-        Season $seasons,
-        Episode $episodes): Response
+    public function showEpisode(Program $program, Season $season, Episode $episode): Response // Renommé "$episodes" en "$episode"
     {
         return $this->render('program/episode_show.html.twig', [
-            'episodes' => $episodes,
-            'seasons' => $seasons,
-            'program' => $program]);
-
+            'episode' => $episode, // Renommé "episodes" en "episode"
+            'season' => $season,
+            'program' => $program
+        ]);
     }
-
-
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,17 +6,25 @@ use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
 {
+    #[Assert\NotBlank]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Ne me laisse pas tout vide !')]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'La catégorie saisie {{value}} ne doit pas dépasser {{limit}} caractères',)]
+
     private ?string $name = null;
+
 
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Program::class)]
     private $programs;
